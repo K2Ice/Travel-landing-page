1a3630d0917b28b6b812c60bc30f63ffa0a7248b
Konrad Korzeniewski <k.korzeniewski94@gmail.com>
2023-02-01 03:09:58 +0100

    Add swiper with functionality

--- swiper.js
+++ swiper.js
@@ -1,4 +1,9 @@
 const opinions = new Swiper(".opinions", {
   slidesPerView: 2,
   spaceBetween: 20,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 })
